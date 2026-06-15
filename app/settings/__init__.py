56ac2f504c7a5939a67043506202d298dc28b1c3
gabrielbaute <gabrielbaute@gmail.com>
2026-06-14 22:03:37 -0400

    Corrección en importación de Tkinter, si falla, se asigna None, para usarse en Docker

--- app/settings/__init__.py
+++ app/settings/__init__.py
@@ -1,7 +1,11 @@
 from app.settings.app_settings import Settings
 from app.settings.log_settings import OctopusLogger
 from app.settings.app_bootstrap import AppBootstrap
-from app.settings.settings_gui import SettingsGUI
 from app.settings.resource_path import get_resource_path
 from app.settings.load_settings import settings
 from app.settings.app_version import __version__
+
+try:
+    from app.settings.settings_gui import SettingsGUI
+except ImportError:
+    SettingsGUI = None
